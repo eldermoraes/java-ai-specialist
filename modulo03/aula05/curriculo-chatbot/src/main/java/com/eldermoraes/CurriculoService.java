3bc5aff47e8809699a65ab2651f41216a7963a5f
Elder Moraes <elder@eldermoraes.com>
2026-04-17 16:14:59 -0300

    Fix

--- modulo03/aula05/curriculo-chatbot/src/main/java/com/eldermoraes/CurriculoService.java
+++ modulo03/aula05/curriculo-chatbot/src/main/java/com/eldermoraes/CurriculoService.java
@@ -1,11 +1,23 @@
 package com.eldermoraes;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
 @RegisterAiService
 public interface CurriculoService {
 
+    @SystemMessage("""
+            Você é um assistente de RH especializado em analisar currículos e extrair informações relevantes sobre os candidatos.
+            Extraia especificamente as informações de acordo com o record Candidato.
+            Não envie nenhum outro tipo de informação na sua resposta, senão vai quebrar a aplicação.
+            As informações que você deverá extrair são:
+            
+            - nome: identifique o nome do candidato nas informações enviadas
+            - idade: identifique a idade do candidato nas informações enviadas
+            - profissaoBase: identifique a profissão do candidato nas informações enviadas
+            - temExperienciaEmNuvem: identifique nas informações enviadas se ele tem experiência com nuvem (retorne true ou false)
+            """)
     @UserMessage("""
         Analise o texto extraído de um currículo e extraia os dados do candidato.
         
